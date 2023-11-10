508b7a5a7a213f70c6264f1485a2c8440eb1fa87
TobiasColer <tcoler@udesa.edu.ar>
2023-11-10 11:51:55 -0300

    Api key y funcion fetch,JSON

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-console.log('OK')
+apiKey = '8b6eae301b66732ee0ec9cb7d499ade8'
+let bestRated =  `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
+
+let fotos = document.querySelector('.fotos')
+let peliculaNombre = document.querySelector('.pelicula1')
+
+
+
+
+fetch(bestRated)
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+
+    let pelicula = data.results[0]
+    peliculaNombre.innerText = pelicula.title
+
+
+
+    console.log(data)
+    return data;
+})
+
+
+.catch(function(error) {
+    console.log(error);
+        
+        
+        
+    return error;
+});
