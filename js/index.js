6ba4049436d3ef50f9068ca8bdaaab25e0f8f138
TobiasColer <tcoler@udesa.edu.ar>
2023-11-17 13:21:56 -0300

    Arreglo detalles

--- js/index.js
+++ js/index.js
@@ -65,12 +65,12 @@ fetch(popSer)
     .then(function (data) {
         for (let i = 5; i < 10; i++) {
             let serie = data.results[i].name
-            let pos = data.results[i].poster_path
+            let pic = data.results[i].poster_path
             let fecha = data.results[i].first_air_date
             fotos[2].innerHTML +=
                 `<div class="foto">
     <h3 class="pelicula1">(${fecha})</h3>
-    <a href="./detail-serie.html?id=${data.results[i].id}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pos}"></a>
+    <a href="detail-serie.html?id=${data.results[i].id}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
     <h3 class="pelicula1">${serie}</h3>
     </div>`
         }
@@ -96,7 +96,7 @@ fetch(bestRated)
             fotos[3].innerHTML +=
                 `<div class="foto">
     <h3 class="pelicula1">(${fecha})</h3>
-    <a href="./detail-movie.html"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
+    <a href="./detail-movie.html?id=${data.results[i].id}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
     <h3 class="pelicula1">${pelicula}</h3>
     </div>`
         }
@@ -120,7 +120,7 @@ fetch(bestRated)
             fotos[4].innerHTML +=
                 `<div class="foto">
     <h3 class="pelicula1">(${fecha})</h3>
-    <a href="./detail-movie.html"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
+    <a href="./detail-movie.html?id=${data.results[i].id}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
     <h3 class="pelicula1">${pelicula}</h3>
     </div>`
         }
@@ -138,15 +138,14 @@ fetch(popSer)
 
     .then(function (data) {
         for (let i = 10; i < 15; i++) {
-            let serieTod = data.results[i].name
-            let pos = data.results[i].poster_path
+            let serie = data.results[i].name
+            let pic = data.results[i].poster_path
             let fecha = data.results[i].first_air_date
-            let idPelicula = data.results[i].id
             fotos[5].innerHTML +=
                 `<div class="foto">
     <h3 class="pelicula1">(${fecha})</h3>
-    <a href="./detail-serie.html?id=${idPelicula}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pos}"></a>
-    <h3 class="pelicula1">${serieTod}</h3>
+    <a href="detail-serie.html?id=${data.results[i].id}"><img class = 'fotos' src="https://image.tmdb.org/t/p/w500${pic}"></a>
+    <h3 class="pelicula1">${serie}</h3>
     </div>`
         }
 
